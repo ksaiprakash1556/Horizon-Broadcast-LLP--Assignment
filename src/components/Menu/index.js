0a9045f6fb2305bbcd4f05bba66e92d3535b919a
saiprakash246 <saiprakash1929@gmail.com>
2024-09-04 18:37:42 +0530

    first commit

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -12,11 +12,11 @@ const translations = {
 };
 
 const imageUrls = {
-  home: 'https://media.istockphoto.com/id/1492732089/photo/hand-presenting-model-home.jpg?s=612x612&w=0&k=20&c=9qKatALD_kGCs5_Gz9pAtrwr-p2ZRBGgbNFco5QEtK0=',
-  movies: 'https://cdn.pixabay.com/photo/2019/04/24/21/55/cinema-4153289_640.jpg',
+  home: 'https://res.cloudinary.com/ksp24/image/upload/v1725454243/Free_download_Sweet_Homes_Wallpaper_Luxury_House_HD_Soft_on_WallpaperSafari_lse26u.jpg',
+  movies: 'https://res.cloudinary.com/ksp24/image/upload/v1725454785/wp8649137-movie-camera-wallpapers_zhcdc7.jpg',
   tvShows: 'https://www.hallaminternet.com/wp-content/uploads/2020/11/8-TV-Shows-and-Movies-all-Marketers-should-watch-HeroImage-1200x514.png',
-  sports: 'https://media.istockphoto.com/id/1141191007/vector/sports-set-of-athletes-of-various-sports-disciplines-isolated-vector-silhouettes-run-soccer.jpg?s=612x612&w=0&k=20&c=SEabW4SHZ7blMHJPxZNSTl_anOMHO3whQI7HIMxFpSg=',
-  live: 'https://allpngfree.com/apf-prod-storage-api/storage/thumbnails/live-streaming-logo-png-images-download-thumbnail-1645119687.jpg',
+  sports: 'https://res.cloudinary.com/ksp24/image/upload/v1725454784/sports-images-for-desktop-background-wallpaper-preview_ee5jvd.jpg',
+  live: 'https://res.cloudinary.com/ksp24/image/upload/v1725454784/wp7442165-live-stream-wallpapers_huv8l5.jpg',
 };
 
 function Menu() {
